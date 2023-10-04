014f4ca71b4c33967e497596dc72cfa79368ffd4
Callinicus <szemetdiszno@gmail.com>
2023-10-05 00:48:01 +0200

    gyak2

--- gyak2.py
+++ gyak2.py
@@ -8,16 +8,17 @@ c = 0 #ez számlál
 
 #print(x) #Ha csalni akarsz leveszed a #t róla.
 
-print("\nGondoltam egy természetes számra ami legfeljebb 100.\nKitalálod melyik az?\n\nTippelj: ", end="")
+print("\nGondoltam egy természetes számra ami legfeljebb 100.\nKitalálod melyik az, legfeljebb 8 próbálkozásból?\n\nTippelj: ", end="")
 
-while y != x:
+while y != x and c < 8:
+    c += 1
     y = int(input())
     if y == 78125:
         y = x #cheatcode ami megnyeri a játátékot =5 * 5 * 5 * 5 * 5 * 5 * 5
         #print(y)
         #print(x)
     if y == 279936: print(x) #cheatcode ami kiírja a helyes választ =6 * 6 * 6 * 6 * 6 * 6 * 6
-    #megoldandó: ha cheatcodeot ír be valaki ne fusson hibára
+    #megoldandó: ha cheatcodeot ír be valaki ne fusson hibára szerk: megoldva
 
     if (y < 1 or y > 100) ^ (y == 279936): raise ValueError("\nCsak 1-től 100-ig terjedő természetes számot tudok elfogadni.")
     # ha jól értem most ez úgy gondolkozik hogy
@@ -28,18 +29,24 @@ while y != x:
     else:
         print("\nSajnos a számod (", end="")
         print(y, end="") #valamiért kénytelen voltam több sorba írni, nem engedte hogy egybekössem, de lehet csak elírtam valamit.
-        print(") nem egyenlő azzal amire gondoltam.\nDe nyugodtan újra próbálhatod!\nAdok egy tippet:\n")
+        print(") nem egyenlő azzal amire gondoltam.\n")
+        if c < 8:
+            print("De nyugodtan újra próbálhatod!\nAdok egy tippet...\n")
         if x < y:
            print("A szám amire gondoltam, kisebb mint: ", end="")
            print(y)
         else:
            print("A szám amire gondoltam, nagyobb mint: ", end="")
            print(y)
-        print("Akkor most próbáld újra: ", end="")
-    c += 1
+    if c < 8 and x != y: print("Akkor most próbáld újra: ", end="")
 
-print("\nSikerült kitalálnod alig", end=" ")
-print(c, end =" ")
-print("próbálkozásból!")
 
-#Ez meglepően egyszerű volt, majd folytatom azzal, hogy felajánlja, hogy melyikőtök találjon ki számot és kitalálja a számod a valószínűleg leggyorsabban.
+if c == 8 and y != x:
+    print("\nSajnálom, elfogytak a próbálkozásaid. : - (")
+    print("Najó megsúgom ez volt: ", x, "\nAzért köszi a játékot! ^^")
+else:
+    print("\nSikerült kitalálnod alig", end=" ")
+    print(c, end =" ")
+    print("próbálkozásból!\nKöszi a játékot! ^^")
+
+#Ez meglepően egyszerű volt (a xorig az már nem), majd folytatom azzal, hogy felajánlja, hogy melyikőtök találjon ki számot és kitalálja a számod a valószínűleg leggyorsabban.
