b9b4793993b3a2a6a5b100e6e79b840eea82de1c
Callinicus <szemetdiszno@gmail.com>
2023-10-30 19:37:51 +0100

    ora

--- ex20.py
+++ ex20.py
@@ -3,13 +3,16 @@ from sys import argv
 script, input_file = argv
 
 def print_all(f):
-    print(f.read())
+    #print(f.read())
+    print(f.readline())
 
 def rewind(f):
     f.seek(0)
+    print(f.seek(0))
 
 def print_a_line(line_count, f):
     print(line_count, f.readline())
+    print(f.tell())
 
 current_file = open(input_file)
 
@@ -26,8 +29,8 @@ print("Let's print three lines:")
 current_line = 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+current_line += 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+current_line += 1
 print_a_line(current_line, current_file)
