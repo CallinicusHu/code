08296e22f216a2ee207239be16b92f7b8b714ee2
Callinicus <szemetdiszno@gmail.com>
2024-08-07 14:16:51 +0200

    dicester -foolingaorund

--- dicetester.py
+++ dicetester.py
@@ -1,42 +1,51 @@
 from random import randint
 
-x_of_d8 = 16
-a_lot = 1 #1_000_000
-sample = 5
+x_of_d8 = max(2, int(input("how many d8s? ")))
+a_lot = int(input("how many rolls? "))
+sample = int(input("how many tries? "))
 
 def d8x():
     #makes a list of xd8 roll outcomes
-    return [randint(1, 8) for _ in range(x_of_d8)]
+    return sorted([randint(1, 8) for _ in range(x_of_d8)])
 
 def high_roller():
     #adds together the highest 2 of xd8 rolls
     roll = d8x()
-    return roll.pop(roll.index(max(roll))) + roll.pop(roll.index(max(roll)))
+    return roll[-1] + roll[-2]
 
 def low_roller():
     #adds together the lowest 2 of xd8 rolls
     roll = d8x()
-    return roll.pop(roll.index(min(roll))) + roll.pop(roll.index(min(roll)))
+    return roll[0] + roll[1]
 
 def roll_high_a_lot():
-    #adds together the highest 2 of xd8 rolls a million times
+    #adds together the highest 2 of xd8 rolls a lot of times
     return [high_roller() for _ in range (a_lot)]
 
 def roll_low_a_lot():
-    # adds together the lowest 2 of xd8 rolls a million times
+    # adds together the lowest 2 of xd8 rolls a lot of times
     return [low_roller() for _ in range(a_lot)]
 
 
 def the_big_number_i_need():
-    #gives me the average of a million rolls
-    lots_of_rolls = roll_high_a_lot()
-    return (sum(lots_of_rolls) / len(lots_of_rolls))
+    #gives me the average of a lot of big rolls
+    ave = (sum(roll_high_a_lot()) / a_lot)
+    rounding = ave - int(ave)
+    roughave = round(ave)
+    if rounding >= 0.7 or rounding <= 0.3:
+        return roughave
+    else:
+        return f"{roughave}-{roughave + 1}"
 
 def the_small_number_i_need():
-    #gives me the average of a million rolls
-    lots_of_rolls = roll_low_a_lot()
-    return (sum(lots_of_rolls) / len(lots_of_rolls))
-
+    #gives me the average of a lot of small rolls
+    ave = (sum(roll_low_a_lot()) / a_lot)
+    rounding = ave - int(ave)
+    roughave = round(ave)
+    if rounding >= 0.7 or rounding <= 0.3:
+        return roughave
+    else:
+        return f"{roughave}-{roughave + 1}"
 
 gimme_high = "\n".join([f"{the_big_number_i_need()}" for _ in range(sample)])
 gimme_low = "\n".join([f"{the_small_number_i_need()}" for _ in range(sample)])
