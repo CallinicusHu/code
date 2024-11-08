69e204f730ce5cedb97efacc7ab32fd8327a203a
Callinicus <szemetdiszno@gmail.com>
2024-11-08 17:18:05 +0100

    more_than_four_months.py javítva.3

--- more_than_four_months.py
+++ more_than_four_months.py
@@ -8,12 +8,12 @@ def more_than_4_months():
     diff = relativedelta(datetime.strptime(date1, '%Y-%m-%d'), datetime.strptime(date2, '%Y-%m-%d'))
     return False if (diff.months < 4 or (diff.months == 4 and diff.days == 0)) else True
 
-print(more_than_120_days())
+print(more_than_4_months())
 
 date2 = '2024-07-08'
 
-print(more_than_120_days())
+print(more_than_4_months())
 
 date2 = '2024-07-09'
 
-print(more_than_120_days())
+print(more_than_4_months())
