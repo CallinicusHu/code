2b5b8fd15071105ae0251ab4baf89cec958a67d1
Callinicus <szemetdiszno@gmail.com>
2025-06-05 19:51:02 +0200

    beosztós 7

--- hhtabor2025olvaso.py
+++ hhtabor2025olvaso.py
@@ -58,6 +58,7 @@ with (open('hhtabor2025info.csv', 'w', newline='') as file):
     writer = csv.writer(file)
     extended_columns = [columns[1], columns[0], columns[2]] + ["Max", "Foglalt", "Szabad"] + columns[3:]
     writer.writerow(extended_columns)
+    counter = []
     for day in raw_data['slots']:
         for session in day['sessions']:
             temp = []
@@ -76,4 +77,59 @@ with (open('hhtabor2025info.csv', 'w', newline='') as file):
             for player in session["players"]:  # játékosok
                 temp.append(f"{player["name"]}")
             writer.writerow(temp)
+            counter += [temp]
+    game_count = len(counter) + 1
+    writer.writerow(["Alább az éjszakai játékokkal nem számolok."])
+    writer.writerow(["Játéknap", "Szombat", "Vasárnap", "Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek"])
+    writer.writerow(["Szabad hely",
+                    f"=SUMIF(A2:A{game_count}, B{game_count + 2}, F2:F{game_count})",
+                    f"=SUMIF(A2:A{game_count}, C{game_count + 2}, F2:F{game_count})",
+                    f"=SUMIF(A2:A{game_count}, D{game_count + 2}, F2:F{game_count})",
+                    f"=SUMIF(A2:A{game_count}, E{game_count + 2}, F2:F{game_count})",
+                    f"=SUMIF(A2:A{game_count}, F{game_count + 2}, F2:F{game_count})",
+                    f"=SUMIF(A2:A{game_count}, G{game_count + 2}, F2:F{game_count})",
+                    f"=SUMIF(A2:A{game_count}, H{game_count + 2}, F2:F{game_count})",
+                    "ennyi játékost tudnak még játékok fogadni aznap."]) #=SUMIF(A2:A63, B65, F2:F63)
+    writer.writerow(["Foglalt hely",
+                    f"=SUMIF(A2:A{game_count}, B{game_count + 2}, E2:E{game_count})",
+                    f"=SUMIF(A2:A{game_count}, C{game_count + 2}, E2:E{game_count})",
+                    f"=SUMIF(A2:A{game_count}, D{game_count + 2}, E2:E{game_count})",
+                    f"=SUMIF(A2:A{game_count}, E{game_count + 2}, E2:E{game_count})",
+                    f"=SUMIF(A2:A{game_count}, F{game_count + 2}, F2:E{game_count})",
+                    f"=SUMIF(A2:A{game_count}, G{game_count + 2}, E2:E{game_count})",
+                    f"=SUMIF(A2:A{game_count}, H{game_count + 2}, E2:E{game_count})",
+                     "ennyi játékhely foglalt aznap."]) #=SUMIF(A2:A63, B65, E2:E63)
+    writer.writerow(["Összes hely",
+                    f"=SUMIF(A2:A{game_count}, B{game_count + 2}, D2:D{game_count})",
+                    f"=SUMIF(A2:A{game_count}, C{game_count + 2}, D2:D{game_count})",
+                    f"=SUMIF(A2:A{game_count}, D{game_count + 2}, D2:D{game_count})",
+                    f"=SUMIF(A2:A{game_count}, E{game_count + 2}, D2:D{game_count})",
+                    f"=SUMIF(A2:A{game_count}, F{game_count + 2}, D2:D{game_count})",
+                    f"=SUMIF(A2:A{game_count}, G{game_count + 2}, D2:D{game_count})",
+                    f"=SUMIF(A2:A{game_count}, H{game_count + 2}, D2:D{game_count})",
+                    "maximális mennyiségű leülthetető játékos."]) #=SUMIF(A2:A63, B65, D2:D63)
 
+    all_the_possible_people = ["Leültethető emberek"]
+    for day in raw_data["slots"]:
+        if len(day["name"]) < 30:
+            todays_people = 0
+            for session in day["sessions"]:
+                todays_people += session["table_count"] * session["table_size"] + session["table_count"]
+            all_the_possible_people.append(f"{todays_people}")
+    all_the_possible_people.append("mesélők + játékosok maximális száma aznap.")
+    writer.writerow(all_the_possible_people)
+
+    people_who_sit = ["Leültetett emberek"]
+    for day in raw_data["slots"]:
+        if len(day["name"]) < 30:
+            todays_people = 0
+            for session in day["sessions"]:
+                todays_people += len(session["players"]) + session["table_count"]
+            people_who_sit.append(f"{todays_people}")
+    people_who_sit.append("mesélők + játékosok aktuális száma aznap.")
+    writer.writerow(people_who_sit)
+
+
+
+# az első oszlop sorai játékos nevek, a további oszlopok sorai játék nevek
+# who_plays_what = ["Játékos", "Szombat", "Vasárnap", "Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat este", "Vasárnap este", "Hétfő este", "Kedd este", "Szerda este", "Csütörtök este", "Péntek este"]
