2ba9981be9d20973b163a70e3972de40aea4344d
Callinicus <szemetdiszno@gmail.com>
2025-11-07 18:57:07 +0100

    cargo 6

--- gyak15_petis.py
+++ gyak15_petis.py
@@ -3,8 +3,8 @@ import json
 
 class Log(object):
     def __init__(self, packages):
-        with open("truck_log", "w", encoding="UTF-8") as truck_log:
-            truck_log.write(f"# Deliver {packages}\n")
+        with open("truck_log_petis", "w", encoding="UTF-8") as truck_log_petis:
+            truck_log_petis.write(f"# Deliver {packages}\n")
 
         self.transport_id = 0
         self.cargo_id = 0
@@ -12,89 +12,84 @@ class Log(object):
     def create_log(self, event, time, transport_id, kind, location, v_destination=None, cargo_id=None,
                    c_destination=None,
                    origin=None):
-        with open("truck_log", "a", encoding="UTF-8") as truck_log:
+        with open("truck_log_petis", "a", encoding="UTF-8") as truck_log_petis:
             if cargo_id and v_destination:
-                truck_log.write(
+                truck_log_petis.write(
                     f'\u007b"event": "{event}", "time": {time}, "transport_id": {transport_id}, "kind": "{kind}", "location": "{location}", "destination": "{v_destination}", "cargo": [\u007b"cargo_id": {cargo_id}, "destination": "{c_destination}", "origin": "{origin}"\u007d]\u007d')
             elif not cargo_id and v_destination:
-                truck_log.write(
+                truck_log_petis.write(
                     f'\u007b"event": "{event}", "time": {time}, "transport_id": {transport_id}, "kind": "{kind}", "location": "{location}", "destination": "{v_destination}"\u007d')
             elif cargo_id and not v_destination:
-                truck_log.write(
+                truck_log_petis.write(
                     f'\u007b"event": "{event}", "time": {time}, "transport_id": {transport_id}, "kind": "{kind}", "location": "{location}", "cargo": [\u007b"cargo_id": {cargo_id}, "destination": "{c_destination}", "origin": "{origin}"\u007d]\u007d')
             elif not cargo_id and not v_destination:
-                truck_log.write(
+                truck_log_petis.write(
                     f'\u007b"event": "{event}", "time": {time}, "transport_id": {transport_id}, "kind": "{kind}", "location": "{location}" \u007d')
-            truck_log.write("\n")
+            truck_log_petis.write("\n")
 
 
 class Truck(object):
-    def __init__(self, name, truck_log):
+    def __init__(self, name, truck_log_petis):
         self.name = name  # callsign of the truck
         self.position = 0  # distance from home
         self.route = None  # A or B route
         self.cargo = False  # it is empty
-        self.truck_log = truck_log
+        self.truck_log_petis = truck_log_petis
         self.my_transport_id = 0
         self.my_cargo_id = 0
         self.sleep = True
 
-    def move(self, packages, worktime):  # packages is list_of_packages
+    def peti_move(self, packages, worktime):
 
+        packages = self.begin_the_turn(packages, worktime)
+        self.take_action()
+        self.end_turn(worktime)
+
+        return packages
+
+    def begin_the_turn(self, packages, worktime):
         if self.position == 0 and packages:
             if packages[0] == "A":
                 self.route = "A"
             elif packages[0] == "B":
                 self.route = "B"
             self.cargo = True
-            self.truck_log.transport_id += 1
-            self.truck_log.cargo_id += 1
-            self.my_cargo_id = self.truck_log.cargo_id
-            self.my_transport_id = self.truck_log.transport_id
-
-            # event, time, transportid, kind, location, destination, cargo
-
-            self.truck_log.create_log(
-                "DEPART", worktime, self.my_transport_id, self.name, "FACTORY", "B" if self.route == "B" else "PORT",
-                self.my_cargo_id, self.route, "FACTORY")
-            self.position = self.position + 1
-            if self.route == "A":
-                self.truck_log.create_log(
-                    "ARRIVE", worktime, self.my_transport_id, self.name, "PORT", None, self.my_cargo_id, "A", "PORT")
+            self.truck_log_petis.transport_id += 1
+            self.truck_log_petis.cargo_id += 1
+            self.my_transport_id = self.truck_log_petis.transport_id
+            self.my_cargo_id = self.truck_log_petis.cargo_id
+            self.sleep = False
             packages = packages[1:]
 
-
-        elif self.route == "B" and self.position != 0:
-            if self.position < 5 and self.cargo:
-                self.position += 1
-                if self.position == 5:
-                    self.cargo = False
-                    self.truck_log.create_log(
-                        "ARRIVE", worktime, self.my_transport_id, self.name, "B", None, self.my_cargo_id, self.route,
-                        "FACTORY")
-            else:
-                if self.position == 5:
-                    self.truck_log.create_log(
-                        "DEPART", worktime, self.my_transport_id, self.name, "B", "FACTORY")
-
-                self.position -= 1
-                if self.position == 0:
-                    self.truck_log.create_log(
-                        "ARRIVE", worktime, self.my_transport_id, self.name, "FACTORY")
-
-        elif self.route == "A" and self.position == 1:
-            self.cargo = False
-            # event, time, transportid, kind, location, destination, cargo
-            self.truck_log.create_log(
-                "DEPART", worktime, self.my_transport_id, self.name, "PORT", "FACTORY")
-            self.position -= 1  # !!!
-            self.truck_log.create_log(
-                "ARRIVE", worktime, self.my_transport_id, self.name, "FACTORY")
-
-        if self.position == 0:
-            self.route = None
+            self.truck_log_petis.create_log(
+                "DEPART", worktime, self.my_transport_id, self.name, "FACTORY", "B" if self.route == "B" else "PORT",
+                self.my_cargo_id, self.route, "FACTORY")
 
         return packages
+
+    def take_action(self):
+        if self.cargo and self.position == 0:
+            self.position += 1
+
+        elif self.position > 0 and not self.cargo:
+            self.position -= 1
+
+        elif self.cargo and self.route == "B" and self.position < 5:
+            self.position += 1
+
+        else:
+            self.sleep = True
+
+    def end_turn(self, worktime):
+        if self.sleep and self.route:
+            if self.position == 0:
+                self.route = None
+                self.truck_log_petis.create_log(
+                    "ARRIVE", worktime, self.my_transport_id, self.name, "FACTORY")
+            if self.position == 1 and self.route == "A":
+                self.cargo = False
+                self.truck_log_petis.create_log(
+                    "ARRIVE", worktime, self.my_transport_id, self.name, "PORT", None, self.my_cargo_id, "A", "PORT")
 
 
 class Ship(Truck):
@@ -113,35 +108,35 @@ class Ship(Truck):
         if not self.cargo and self.port and self.position == 0:
             self.cargo = True
             self.port = self.port[1:]
-            self.truck_log.transport_id += 1
-            self.my_transport_id = self.truck_log.transport_id
-            self.truck_log.create_log(
+            self.truck_log_petis.transport_id += 1
+            self.my_transport_id = self.truck_log_petis.transport_id
+            self.truck_log_petis.create_log(
                 "DEPART", worktime, self.my_transport_id, self.name, "PORT", "A", self.my_cargo_id, "A", "PORT")
 
         elif not self.cargo and self.position > 0:
             if self.position == 4:
-                self.truck_log.create_log(
+                self.truck_log_petis.create_log(
                     "DEPART", worktime, self.my_transport_id, self.name, "A", "PORT")
             self.position -= 1
             if self.position == 0:
-                self.truck_log.create_log(
+                self.truck_log_petis.create_log(
                     "ARRIVE", worktime, self.my_transport_id, self.name, "PORT")
 
         if self.position != 4 and self.cargo:
             self.position += 1
 
         if self.position == 4 and self.cargo:
-            self.truck_log.create_log(
+            self.truck_log_petis.create_log(
                 "ARRIVE", worktime, self.my_transport_id, self.name, "A", None, self.my_cargo_id, "A", "PORT")
             self.cargo = False
 
 
 def delivery_timer(list_of_packages):
-    truck_log = Log(list_of_packages)
+    truck_log_petis = Log(list_of_packages)
     worktime = 0
-    truck_1 = Truck("truck_1", truck_log)
-    truck_2 = Truck("truck_2", truck_log)
-    ship_1 = Ship(truck_log)
+    truck_1 = Truck("truck_1", truck_log_petis)
+    truck_2 = Truck("truck_2", truck_log_petis)
+    ship_1 = Ship(truck_log_petis)
 
     while True:
 
@@ -152,12 +147,12 @@ def delivery_timer(list_of_packages):
         if len(list_of_packages) > 0 and not (list_of_packages[0] == "A" or list_of_packages[0] == "B"):
             return f"error at worktime {worktime} invalid package in {list_of_packages}"
 
-        list_of_packages = truck_1.move(list_of_packages, worktime)
+        list_of_packages = truck_1.peti_move(list_of_packages, worktime)
 
         if len(list_of_packages) > 0 and not (list_of_packages[0] == "A" or list_of_packages[0] == "B"):
             return f"error at worktime {worktime} invalid package in {list_of_packages}"
 
-        list_of_packages = truck_2.move(list_of_packages, worktime)
+        list_of_packages = truck_2.peti_move(list_of_packages, worktime)
 
         print()
         print(truck_1.name, "pos", truck_1.position, "cargo", truck_1.cargo, "route", truck_1.route, "worktime",
---
+++ test_gyak15_petis.py
@@ -0,0 +1,147 @@
+import gyak15_petis
+
+
+def test_delivery_time_a():
+    a = gyak15_petis.delivery_timer("A")
+
+    assert a == 5
+
+
+def test_delivery_time_aa():
+    aa = gyak15_petis.delivery_timer("AA")
+
+    assert aa == 13
+
+
+def test_delivery_time_aaa():
+    aaa = gyak15_petis.delivery_timer("AAA")
+
+    assert aaa == 21
+
+
+def test_delivery_time_aaaa():
+    aaaa = gyak15_petis.delivery_timer("AAAA")
+
+    assert aaaa == 29
+
+
+def test_delivery_time_b():
+    b = gyak15_petis.delivery_timer("B")
+
+    assert b == 5
+
+
+def test_delivery_time_bb():
+    bb = gyak15_petis.delivery_timer("BB")
+
+    assert bb == 5
+
+
+def test_delivery_time_bbb():
+    bbb = gyak15_petis.delivery_timer("BBB")
+
+    assert bbb == 15
+
+
+def test_delivery_time_bbbbbbb():  # 7b
+
+    bbbbbbb = gyak15_petis.delivery_timer("BBBBBBB")
+
+    assert bbbbbbb == 35
+
+
+def test_wrongpack():
+    wrong = gyak15_petis.delivery_timer("BBBMBB5")
+
+    assert wrong == "error at worktime 11 invalid package in MBB5"
+
+
+def test_delivery_time_ab():
+    ab = gyak15_petis.delivery_timer("AB")
+
+    assert ab == 5
+
+
+def test_delivery_time_abb():
+    abb = gyak15_petis.delivery_timer("ABB")
+
+    assert abb == 7
+
+
+def test_delivery_time_aab():
+    aab = gyak15_petis.delivery_timer("AAB")
+
+    assert aab == 13
+
+
+def test_delivery_time_aba():
+    aba = gyak15_petis.delivery_timer("ABA")
+
+    assert aba == 13
+
+
+def test_delivery_time_bab():
+    bab = gyak15_petis.delivery_timer("BAB")
+
+    assert bab == 7
+
+
+def test_delivery_time_aababbab():
+    aababbab = gyak15_petis.delivery_timer("AABABBAB")
+
+    assert aababbab != "faszsetudja"
+
+
+def test_delivery_time_abbbabaaabbb():
+    abbbabaaabbb = gyak15_petis.delivery_timer("ABBBABAAABBB")
+
+    assert abbbabaaabbb != "faszsetudja"
+
+
+def test_a_log():
+    gyak15_petis.delivery_timer("A")
+    good = open("deliver_a_good.txt").readlines()
+    print(good)
+    maybe = open("truck_log_petis").readlines()
+    print(maybe)
+    assert good == maybe
+
+
+def test_a_log():
+    gyak15_petis.delivery_timer("A")
+    good = open("deliver_a_good.txt").readlines()
+    maybe = open("truck_log_petis").readlines()
+    assert good == maybe
+
+
+def test_to_fail_log():
+    gyak15_petis.delivery_timer("AB")
+    good = open("deliver_a_good.txt").readlines()
+    maybe = open("truck_log_petis").readlines()
+    assert good != maybe
+
+
+def test_b_log():
+    gyak15_petis.delivery_timer("B")
+    good = open("deliver_b_good.txt").readlines()
+    maybe = open("truck_log_petis").readlines()
+    assert good == maybe
+
+def test_ab_log():
+    gyak15_petis.delivery_timer("AB")
+    good = open("deliver_ab_good.txt").readlines()
+    maybe = open("truck_log_petis").readlines()
+    assert good == maybe
+
+
+def test_baba_log():
+    gyak15_petis.delivery_timer("BABA")
+    good = open("deliver_baba_good.txt").readlines()
+    maybe = open("truck_log_petis").readlines()
+    assert good == maybe
+
+def test_long_log():
+    gyak15_petis.delivery_timer("BABAABBBAAAAAAAA")
+    good = open("deliver_long_good.txt").readlines()
+    maybe = open("truck_log_petis").readlines()
+    assert good == maybe
